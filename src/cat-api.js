19ce2c85c66a6dfe3adc22100474ce622ed05780
michallow <michallo_w@o2.pl>
2024-02-13 19:29:06 +0100

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -45,10 +45,20 @@ export async function fetchCatByBreed(breedId) {
 
 export async function renderCatInfo(cat) {
   const catInfoContainer = document.querySelector('.cat-info');
-  catInfoContainer.innerHTML = `
-      <p><b>Name:</b> ${cat.name}</p>
-      <p><b>Description:</b> ${cat.description}</p>
-      <p><b>Temperament:</b> ${cat.temperament}</p>
-      <img src="https://cdn2.thecatapi.com/images/${cat.reference_image_id}" alt="Cat Image">
-    `;
+  if (cat.reference_image_id) {
+    catInfoContainer.innerHTML = `
+        <p><b>Name:</b> ${cat.name}</p>
+        <p><b>Description:</b> ${cat.description}</p>
+        <p><b>Temperament:</b> ${cat.temperament}</p>
+        <img src="https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg" alt="Cat Image">
+      `;
+  } else {
+    console.log('No image found');
+    catInfoContainer.innerHTML = `
+        <p><b>Name:</b> ${cat.name}</p>
+        <p><b>Description:</b> ${cat.description}</p>
+        <p><b>Temperament:</b> ${cat.temperament}</p>
+        <p>No image found</p>
+      `;
+  }
 }
