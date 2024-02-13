812cff7eed35484ce955dcb16257be604e9b913f
michallow <michallo_w@o2.pl>
2024-02-13 19:25:57 +0100

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -46,8 +46,9 @@ export async function fetchCatByBreed(breedId) {
 export async function renderCatInfo(cat) {
   const catInfoContainer = document.querySelector('.cat-info');
   catInfoContainer.innerHTML = `
-        <p><b>Name:</b> ${cat.name}</p>
-        <p><b>Description:</b> ${cat.description}</p>
-        <p><b>Temperament:</b> ${cat.temperament}</p>
-      `;
+      <p><b>Name:</b> ${cat.name}</p>
+      <p><b>Description:</b> ${cat.description}</p>
+      <p><b>Temperament:</b> ${cat.temperament}</p>
+      <img src="https://cdn2.thecatapi.com/images/${cat.reference_image_id}" alt="Cat Image">
+    `;
 }
