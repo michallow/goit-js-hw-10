3a7d5d3490a672836c30309859c3372c401ac8ea
michallow <michallo_w@o2.pl>
2024-02-13 19:21:35 +0100

    upd

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,8 +3,8 @@ const api_key =
   'live_6XgfHVWeHr1Vp0Qc9jpmIcMoHZ9L9uTquqcjtkyD5VB41vjdTvxGn1USKmOULZT9';
 let storedBreeds = [];
 
-const fetchCatByBreedSelector = document.querySelector('breed-select');
-const catInfo = document.querySelector('cat-info');
+export const breedSelect = document.querySelector('.breed-select');
+export const catInfo = document.querySelector('.cat-info');
 
 export function fetchBreeds() {
   return fetch(url, {
@@ -35,31 +35,19 @@ export function fetchBreeds() {
     });
 }
 
-fetchCatByBreedSelector.addEventListener('change', () => {
-  const selectedBreedId = fetchCatByBreedSelector.value;
-  fetchCatByBreed(selectedBreedId)
-    .then(cat => renderCatInfo(cat))
-    .catch(error => console.log('Error fetching cat by breed:', error));
-});
-
 export async function fetchCatByBreed(breedId) {
-  const breedUrl = `https://api.thecatapi.com/v1/breeds/${breedId}`;
-  return fetch(breedUrl, {
-    headers: {
-      'x-api-key': api_key,
-    },
-  }).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  const selectedBreed = storedBreeds.find(breed => breed.id === breedId);
+  if (!selectedBreed) {
+    throw new Error(`Breed with id ${breedId} not found`);
+  }
+  return selectedBreed;
 }
 
-function renderCatInfo(cat) {
-  catInfo.innerHTML = `
-      <p><b>Name:</b> ${cat.name}</p>
-      <p><b>Description:</b> ${cat.description}</p>
-      <p><b>Temperament:</b> ${cat.temperament}</p>
-    `;
+export async function renderCatInfo(cat) {
+  const catInfoContainer = document.querySelector('.cat-info');
+  catInfoContainer.innerHTML = `
+        <p><b>Name:</b> ${cat.name}</p>
+        <p><b>Description:</b> ${cat.description}</p>
+        <p><b>Temperament:</b> ${cat.temperament}</p>
+      `;
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,20 @@
 import axios from 'axios';
-import { fetchBreeds } from './cat-api.js';
-import { fetchCatByBreed } from './cat-api.js';
+import {
+  fetchBreeds,
+  fetchCatByBreed,
+  breedSelect,
+  renderCatInfo,
+  catInfo,
+} from './cat-api.js';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_6XgfHVWeHr1Vp0Qc9jpmIcMoHZ9L9uTquqcjtkyD5VB41vjdTvxGn1USKmOULZT9';
 
-const breedSelect = document.querySelector('.breed-select');
-const breedId = breedSelect.value;
+breedSelect.addEventListener('change', () => {
+  const breedId = breedSelect.value;
+  fetchCatByBreed(breedId)
+    .then(cat => renderCatInfo(cat))
+    .catch(error => console.log('Error fetching cat by breed:', error));
+});
 
 fetchBreeds();
-
-// .then(breeds => {
-//   const breedSelect = document.querySelector('.breed-select');
-//   const loader = document.querySelector('.loader');
-
-//   loader.style.display = 'none';
-
-//   breeds.forEach(breed => {
-//     const option = document.createElement('option');
-//     option.value = breed.id;
-//     option.textContent = breed.name;
-//     breedSelect.appendChild(option);
-//   });
-// })
-// .catch(error => {
-//   const errorText = document.querySelector('.error');
-//   errorText.style.display = 'block';
-//   console.error('Error fetching breeds:', error);
-// });
-
-fetchCatByBreed(breedId);
